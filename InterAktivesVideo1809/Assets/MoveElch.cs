e7ea538bd4b57be6fd361396e33c1a6432395d96
bizonAI <d.robra@online.de>
2017-09-27 11:06:11 +0200

    new5

---
+++ InterAktivesVideo1809/Assets/MoveElch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MoveElch : MonoBehaviour
+{
+    public Transform player;
+
+    public float actionTriggerDistance;
+    public float moveSpeed = 1;
+
+    [SerializeField]
+    float showDistance;
+
+    Rigidbody rigid;
+    float distance;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        if (distance <= actionTriggerDistance)
+        {
+            rigid.velocity = transform.right * -moveSpeed;
+        }
+
+
+
+        distance = Vector3.Distance(transform.position, player.position);
+
+        //Debug.Log(distance);
+
+        showDistance = distance;
+    }
+}
